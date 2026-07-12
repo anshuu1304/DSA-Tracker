dd366e5f2ab8eb18a2adde2738dfd31a33395da7
Anshuu Shukla <shuklaluv83@gmail.com>
2026-07-12 11:24:28 +0530

    Time: 663 ms (34.48%), Space: 22.5 MB (18.3%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,34 +1,36 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
-        
         nums.sort()
-        result = []
+        n = len(nums)
+        res = []
 
-        for i in range(len(nums)):
-            if i >0 and nums[i] == nums[i-1]:
+        for i in range(n-2):
+
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
 
-            if nums[i] > 0:
-                break
+            left = i+1
+            right = n-1
 
-            left = i + 1
-            right = len(nums) - 1
-        
             while left < right:
-                total = nums[i] + nums[left] + nums[right]
-                if total < 0:
+
+                total = nums[i]+nums[left]+nums[right]
+
+                if total == 0:
+                    res.append([nums[i], nums[left], nums[right]])
                     left+=1
-                elif total >0:
-                    right -=1
-                elif total == 0:
-                    result.append([nums[i] , nums[left] , nums[right]])
-
-                    while left < right and nums[left] == nums[left+1]:
-                        left+=1
-                    while left < right and nums[right] == nums[right-1]:
-                        right-=1    
+                    right-=1   
                     
-                    left +=1
-                    right -=1 
+                    while left < right and nums[left] == nums[left-1]:
+                        left+=1
+                        
+                    while left < right and nums[right] == nums[right+1]:
+                        right-=1
 
-        return result                   
+                elif total > 0:
+                    right -=1
+                else:
+                    left +=1
+
+        return res                            
+        
