aadcbb2e006795d95db7f6bb3bab489a4a765155
Anshuu Shukla <shuklaluv83@gmail.com>
2026-07-18 17:03:29 +0530

    Time: 274 ms (97.09%), Space: 0B (100%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 SELECT w.name , w.population , w.area
 FROM World w
-HAVING w.area >= 3000000 OR w.population >= 25000000;
+WHERE w.area >= 3000000 OR w.population >= 25000000;
