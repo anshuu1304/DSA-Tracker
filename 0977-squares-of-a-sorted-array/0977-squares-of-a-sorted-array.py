8d10569bb720b39ae1cf0a4d713d4f62fddb6a1c
Anshuu Shukla <shuklaluv83@gmail.com>
2026-08-07 15:28:08 +0530

    Time: 7 ms (77.68%), Space: 21.3 MB (9.68%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,20 +1,46 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        
-        n = len(nums)
-        res=[0]*n
-        left =0
-        right = n-1
-        idx = n-1
 
-        while left <= right:
-            if abs(nums[left]) > abs(nums[right]):
-                res[idx] = nums[left]*nums[left]
-                left +=1
+        pos =[]
+        neg =[]
+
+        for num in nums:
+            if num >=0:
+                pos.append(num)
             else:
-                res[idx] = nums[right]*nums[right]
-                right -=1
+                neg.append(num)
 
-            idx -=1
+        for i in range(len(pos)):
+            pos[i] = pos[i]*pos[i]
+        for j in range(len(neg)):
+            neg[j] = neg[j]*neg[j]
 
-        return res        
+        neg.reverse()
+
+        i=0
+        j=0
+        res = [] 
+        m = len(pos)
+        n = len(neg)
+
+        while i<m and j<n:
+
+            if pos[i] <= neg[j]:
+                res.append(pos[i])
+                i+=1
+            else:
+                res.append(neg[j])
+                j+=1
+
+        while i<m:
+            res.append(pos[i])
+            i+=1
+
+        while j<n:
+            res.append(neg[j])
+            j+=1
+                    
+        return res            
+
+                   
+        
