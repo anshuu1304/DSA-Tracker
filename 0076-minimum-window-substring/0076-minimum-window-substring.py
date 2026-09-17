5f07e039d04efc2301d07c382e2a2b536d68132f
Anshuu Shukla <shuklaluv83@gmail.com>
2026-09-17 15:23:51 +0530

    Time: 130 ms (27.76%), Space: 19.7 MB (74.13%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -5,42 +5,38 @@ class Solution:
         res = ""
         min_length = float('inf')
 
-        freq1= {}
-        freq2 ={}
+        freq1 = {}
+        freq2 = {}
         count = 0
 
         for char in t:
             freq2[char] = freq2.get(char , 0)+1
 
-
         for high in range(len(s)):
+            freq1[s[high]] = freq1.get(s[high] , 0)+1
 
-            freq1[s[high]] = freq1.get(s[high],0)+1
-
-            if s[high] in freq2 and  freq1[s[high]] <= freq2[s[high]]:
+            if s[high] in freq2 and freq1[s[high]] <= freq2[s[high]]:
                 count+=1
-            
+
             while count == len(t):
 
-                length = high - low+1
+                length = high - low +1
 
-                if length <= min_length:
-
+                if length < min_length:
                     min_length = length
                     res = s[low:high+1]
 
-
-                if s[low] in freq2  and freq1[s[low]] <= freq2[s[low]]:
-                    count -=1
+                if s[low] in freq2 and freq1[s[low]] <= freq2[s[low]]:
+                    count-=1 
 
                 freq1[s[low]] -=1
 
-                if  freq1[s[low]] == 0:
+                if freq1[s[low]] == 0:
                     del freq1[s[low]]
 
                 low+=1
 
-        return res               
+        return res            
 
 
-            
+        
