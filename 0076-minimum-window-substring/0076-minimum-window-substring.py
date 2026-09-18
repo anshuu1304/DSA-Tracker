3d6c68be38f063d894920eb7815a4d246a382cf2
Anshuu Shukla <shuklaluv83@gmail.com>
2026-09-18 11:29:53 +0530

    Time: 130 ms (27.69%), Space: 19.7 MB (74.1%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,9 +1,9 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
 
-        low = 0
+        low = 0 
         res = ""
-        min_length = float('inf')
+        min_len = float('inf')
 
         freq1 = {}
         freq2 = {}
@@ -13,30 +13,31 @@ class Solution:
             freq2[char] = freq2.get(char , 0)+1
 
         for high in range(len(s)):
+
             freq1[s[high]] = freq1.get(s[high] , 0)+1
 
             if s[high] in freq2 and freq1[s[high]] <= freq2[s[high]]:
-                count+=1
+
+                count +=1
 
             while count == len(t):
 
-                length = high - low +1
+                length = high-low+1
 
-                if length < min_length:
-                    min_length = length
+                if length < min_len:
+                    min_len = length
                     res = s[low:high+1]
 
                 if s[low] in freq2 and freq1[s[low]] <= freq2[s[low]]:
-                    count-=1 
 
-                freq1[s[low]] -=1
+                    count-=1
 
+                freq1[s[low]] -= 1
+                
                 if freq1[s[low]] == 0:
-                    del freq1[s[low]]
+                    del  freq1[s[low]]   
 
                 low+=1
-
-        return res            
-
-
+                
+        return res                
         
